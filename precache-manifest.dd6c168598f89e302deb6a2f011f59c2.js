a38e4bed8daa61af35e6dda5521ab4661985e13f
lvsuzhen <1205521940@qq.com>
2019-08-21 10:57:41 +0800

    Updates

---
+++ precache-manifest.dd6c168598f89e302deb6a2f011f59c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcadb63b13aa2e4c5e2d12d2800a844e",
+    "url": "/react-helloworld/myapp/build/index.html"
+  },
+  {
+    "revision": "a7e9010831a885bb9d95",
+    "url": "/react-helloworld/myapp/build/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "dd8bdc80d8cc7d23f600",
+    "url": "/react-helloworld/myapp/build/static/js/2.9a82b346.chunk.js"
+  },
+  {
+    "revision": "a7e9010831a885bb9d95",
+    "url": "/react-helloworld/myapp/build/static/js/main.38fea45b.chunk.js"
+  },
+  {
+    "revision": "41afa21e5beae67944fb",
+    "url": "/react-helloworld/myapp/build/static/js/runtime~main.a93a2db5.js"
+  }
+]);
