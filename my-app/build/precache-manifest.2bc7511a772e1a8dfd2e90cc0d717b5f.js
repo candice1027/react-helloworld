9782765382f8ad0b39fbb451106a5e3d3b2e42fa
lvsuzhen <1205521940@qq.com>
2019-08-21 10:43:49 +0800

    build

---
+++ my-app/build/precache-manifest.2bc7511a772e1a8dfd2e90cc0d717b5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dccbea70e306b0e58a3d8677fa5daa71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21d42ccb0322d775f250",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "dd8bdc80d8cc7d23f600",
+    "url": "/static/js/2.9a82b346.chunk.js"
+  },
+  {
+    "revision": "21d42ccb0322d775f250",
+    "url": "/static/js/main.9f0245b1.chunk.js"
+  },
+  {
+    "revision": "9488af1e8a466321b589",
+    "url": "/static/js/runtime~main.c5c0a44e.js"
+  }
+]);
