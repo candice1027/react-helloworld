67497f62f61f0757852dcbeb14a642227d877d47
lvsuzhen <1205521940@qq.com>
2019-08-21 14:01:33 +0800

    增加button组件

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Button from './button'
 
 function App() {
   return (
     <div className="App">
+      <Button ></Button>
+      <Button ></Button>
       <header className="App-header">
         This is my first react-demo
       </header>
---
+++ my-app/src/button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './button.css'
+function Button() {
+    return (
+        <div class="button">
+            <button >这里是按钮</button>
+        </div> 
+    )
+}
+export default Button
